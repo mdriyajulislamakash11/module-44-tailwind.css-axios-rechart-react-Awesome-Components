beda7c9a57613732de2282e27bec6f6cb0c7ce89
Akash <hafejmohammadakash@gmail.com>
2025-04-17 19:31:06 +0600

    projects setup react, tailwind, daisyUL

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-2xl font-bold'>My Awesome React Components</h1>
+      
+    </>
+  )
+}
+
+export default App
