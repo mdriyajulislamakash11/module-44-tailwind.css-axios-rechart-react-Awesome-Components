7589ac960ac27a2ff0b7950a72eb7a4cb187b269
Akash <hafejmohammadakash@gmail.com>
2025-04-17 23:17:13 +0600

    enubar toggle korar niyom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import NavBar from './components/NavBar'
+
 
 
 function App() {
 
   return (
     <>
-      
+
+      <NavBar />
       <h1 className='text-2xl font-bold'>My Awesome React Components</h1>
       
     </>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
+import Link from "./Link";
+import { IoClose, IoMenu } from "react-icons/io5";
 
 const NavBar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [open, setOpen] = useState(false);
+
+  const routes = [
+    {
+      id: 1,
+      path: "/home",
+      name: "Home",
+    },
+    {
+      id: 2,
+      path: "/about",
+      name: "About Us",
+    },
+    {
+      id: 3,
+      path: "/services",
+      name: "Services",
+    },
+    {
+      id: 4,
+      path: "/contact",
+      name: "Contact",
+    },
+    {
+      id: 5,
+      path: "/login",
+      name: "Login",
+    },
+  ];
+
+  return (
+    <nav>
+      <div className="p-8 md:hidden text-2xl " onClick={() => setOpen(!open)}>
+        {open == true ? <IoMenu className="md:hidden" /> : <IoClose />}
+        
+      </div>
+      <ul className="md:flex items-center absolute md:static">
+        {routes.map((route) => (
+          <Link key={route.id} route={route} />
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
